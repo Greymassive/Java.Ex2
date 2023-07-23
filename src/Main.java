f6359c126a3a538f855106334af1a56f66ea13a3
Greymassive <padiktolya@gmail.com>
2023-07-23 16:54:07 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int cost = 12365;
+        int mile = 20;
+
+        int bonus = cost / mile;
+        System.out.println("Your number of miles is " + bonus);
+    }
+}
